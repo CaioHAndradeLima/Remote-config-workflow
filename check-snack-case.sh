89bd0583221d63ee8bc2afa755d809eb8a777526
Caio Henrique <caiohenrique@Caios-MacBook-Air-2.local>
2025-05-21 17:38:08 -0300

    creating a way to check snack case pattern using bash script

---
+++ check-snack-case.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#!/bin/bash
+
+# Function to check if a string is snake_case
+is_snake_case() {
+  [[ "$1" =~ ^[a-z]+(_[a-z0-9]+)*$ ]]
+}
+
+# Function to check if a list of strings is snake_case
+is_list_snake_case() {
+  local json_array="$1"
+  local all_valid=true
+
+  mapfile -t items < <(echo "$json_array" | jq -r '.[]')
+
+  for item in "${items[@]}"; do
+    if ! is_snake_case "$item"; then
+      all_valid=false
+      break
+    fi
+  done
+
+  if $all_valid; then
+    return 0
+  else
+    return 1
+  fi
+}
